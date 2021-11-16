ff4c445e7993e54dfde0fa04de2a94ace68d80b1
Seungju Ham <hsj1514@kaist.ac.kr>
2021-11-17 02:20:42 +0900

    typo

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.rendger('index', { title: 'Express' });
+  res.render('index', { title: 'Express' });
 });
 
 router.get('/test', function(req, res, next) {
