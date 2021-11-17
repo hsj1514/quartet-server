8168c59a7479254b1698203b422ff6480a8aa0ad
YoungsterJohnny <yohanme@kaist.ac.kr>
2021-11-18 02:26:09 +0900

    Revert "[feature] add name and state updater"
    This reverts commit b1e7c92b2983bde1b3d8077ee905cca99bd73e8b.

--- routes/index.js
+++ routes/index.js
@@ -6,16 +6,19 @@ var Performer={
 }
 const actions = {
 	NOTHING: 0,
-  CLAP: 1,
-  GROUPJUMP: 2
+	HEART: 1,
+	JUMP: 2,
+	GLOW: 3,
+  CLAP: 4,
+  GROUPJUMP: 5
 }
 var Player1={
-  currentAction: actions.NOTHING,
-  name: ""
+  id: 0,
+  currentAction: actions.NOTHING
 }
 var Player2={
-  currentAction: actions.NOTHING,
-  name: ""
+  id: 0,
+  currentAction: actions.NOTHING
 }
 
 /* GET home page. */
@@ -23,48 +26,18 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/name', function(req, res, next) {
-  if(req.query.name!=Player2.name && req.query.name!=""){
-    Player1.currentAction=Player2.currentAction;
-    Player1.name=Player2.name;
-    Player2.name=req.query.name;
-    Player2.currentAction=actions.NOTHING;
-  }
-});
-
 router.get('/test', function(req, res, next) {
   Performer.isPlaying=false;
-  Performer.broadcast="";
+  
   res.send('quartet server test');
 });
 
 router.get('/update', function(req, res, next) {
-  if(req.query.isAudience){
-    if(req.query.name==Player2.name){
-      res.send({
-        isPlaying: Performer.isPlaying,
-        broadcast: Performer.broadcast,
-        action: Player1.currentAction,
-        name: Player1.name,
-      });
-    }
-    else{
-      res.send({
-        isPlaying: Performer.isPlaying,
-        broadcast: Performer.broadcast,
-        action: Player2.currentAction,
-        name: Player2.name,
-      });
-    }
-  }
-  else{
-    res.send({
-      p1state: Player1.currentAction,
-      p1name: Player1.name,
-      p2state: Player2.currentAction,
-      p2name: Player2.name,
-    });
-  }
+  res.send({
+    isPlaying: Performer.isPlaying,
+    broadcast: Performer.broadcast,
+    action: Player1.currentAction
+  });
 });
 
 router.get('/play', function(req, res, next) {
@@ -85,6 +58,4 @@ router.get('/broadcast', function(req, res, next) {
     res.send('Not a Performer');
   }
 });
-
-
 module.exports = router;
