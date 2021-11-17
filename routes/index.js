6cce0ba09560c65803c8455abe53b4d7fdda7772
YoungsterJohnny <yohanme@kaist.ac.kr>
2021-11-17 16:52:49 +0900

    [feature] receive broadcast message from client

--- routes/index.js
+++ routes/index.js
@@ -33,18 +33,23 @@ router.get('/test', function(req, res, next) {
 router.get('/update', function(req, res, next) {
   res.send({
     isPlaying: Performer.isPlaying,
-    action: Player1.currentAction
+    action: Player1.currentAction,
+    broadcast: Performer.broadcast
   });
 });
+
 router.get('/play', function(req, res, next) {
   if(!req.query.isAuidence){
     Performer.isPlaying=req.query.isPlaying;
-    if(req.query.isPlaying){
-      res.send('playing');
-    }
-    else{
-      res.send('paused');
-    }
+  }
+  else{
+    res.send('Not a Performer');
+  }
+});
+
+router.get('/broadcast', function(req, res, next) {
+  if(!req.query.isAuidence){
+    Performer.broadcast=req.query.broadcast;
   }
   else{
     res.send('Not a Performer');
