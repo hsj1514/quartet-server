54cdc7371daf50399bc958cc37f139f888cea70f
YoungsterJohnny <yohanme@kaist.ac.kr>
2021-11-17 14:22:35 +0900

    [feature] send client music play variable

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-
+var isPlaying=false;
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
@@ -10,4 +10,18 @@ router.get('/test', function(req, res, next) {
   res.send('quartet server test');
 });
 
+router.get('/update', function(req, res, next) {
+  res.send({
+    isPlaying: isPlaying
+  });
+});
+router.get('/play', function(req, res, next) {
+  isPlaying=req.isPlaying;
+  if(isPlaying){
+  res.send('success');
+  }
+  else{
+  res.send('fail');
+  }
+});
 module.exports = router;
